7a186bf19c891076fb3791c3b8eed47d8e93fce7
kitithoughts-bit <kitithoughts@gmail.com>
2026-07-24 13:20:32 +0700

    Initial commit: Setup Spring Boot project

---
+++ miniproj/src/main/java/com/example/miniproj/MiniprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojApplication.class, args);
+	}
+
+}
