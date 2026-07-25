709f1d8739a7f12a42ad3ed61ba409f123e8b429
Nadia <nordorja99@gmail.com>
2026-07-25 13:01:36 +0700

    Coffee class is done

--- miniproj/src/main/java/com/example/miniproj/model/Coffee.java
+++ miniproj/src/main/java/com/example/miniproj/model/Coffee.java
@@ -1,3 +1,40 @@
 public class Coffee {
+    private Long id;
+    private String name;
+    private double price;
+
+    public Coffee(){
+
+    }
+
+    public Coffee(Long id, String name, double price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId(){
+        return this.id;
+    }
     
+    public String getName(){
+        return this.name;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
 }
